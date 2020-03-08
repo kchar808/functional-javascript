8bc6d079fba2e21f585fa52bc7118dea0f5d11a5
Keelana Char <keelanachar@gmail.com>
2020-03-07 18:12:12 -0600

    Higher Order Functions

--- program.js
+++ program.js
@@ -1,5 +1,15 @@
-function upperCaser(word) {
-  return word.toUpperCase();
+// function upperCaser(word) {
+//   return word.toUpperCase();
+// };
+
+// module.exports = upperCaser;
+
+function repeat(fn, num) {
+  fn = function loopDeLoop() {
+    for(let i = 0; i <= num; i++) {
+      return "HEY!"
+    }
+  };
 };
 
-module.exports = upperCaser;
+module.exports = repeat;
