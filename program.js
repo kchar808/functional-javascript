8e917e00ed1333df4dc0bebaa544c8c606261e37
Keelana Char <keelanachar@gmail.com>
2020-03-07 18:54:28 -0600

    Basic: Map

--- program.js
+++ program.js
@@ -4,12 +4,18 @@
 
 // module.exports = upperCaser;
 
-function repeat(fn, num) {
-  fn = function loopDeLoop() {
-    for(let i = 0; i <= num; i++) {
-      return "HEY!"
-    }
-  };
-};
+// function repeat(fn, num) {
+//   fn = function loopDeLoop() {
+//     for(let i = 0; i <= num; i++) {
+//       return "HEY!"
+//     }
+//   };
+// };
 
-module.exports = repeat;
+// module.exports = repeat;
+
+module.exports = function doubleAll(numbers) {
+  var result = [];
+  result = numbers.map(numbers => numbers * 2);
+  return result;
+};
